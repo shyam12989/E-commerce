149b09a21bf11c311af74e785414ac594c82f3de
shyam <shyam.dpce@gmail.com>
2025-02-22 13:55:31 +0530

    First Commit for Product Listings

--- src/Component/ListOfProducts.js
+++ src/Component/ListOfProducts.js
@@ -3,13 +3,20 @@ import { useEffect, useState } from "react";
 function ListOfProducts() {
 
     const [data, setData] = useState([]);
+    const [Limit, setLimit]=useState(5);
+    
 
     useEffect(() => {
-        fetch("https://fakestoreapi.com/products")
+        fetch(`https://fakestoreapi.com/products?limit=${Limit}`)
             .then(response => response.json())
             .then(data => setData(data))
             .catch(error => console.error(error))
-    }, [])
+    }, [Limit]) 
+    
+    const increaseLimit=()=>{
+        setLimit(previousValue=>previousValue+5);    
+
+    }
 
     const TableHeaders = () => (
         <thead>
@@ -24,16 +31,20 @@ function ListOfProducts() {
     );
     
     const ProductValues = data.map((values) => (
+
         <tr>
             <td>{values.id}</td>
             <td>{values.title}</td>
             <td>{values.price}</td>
             <td>{values.category.toUpperCase()}</td>
-            <td><img style={{width:"30%", height:"10%"}} src={values.image} alt="Item image"></img></td>
+            <td><img style={{width:"20%"}} src={values.image} alt="itempic"></img></td>
         </tr>
     ));    
     return (
         <>
+            <div style={{paddingLeft:"85%", paddingBottom:"10px"}}>                
+                <button onClick={increaseLimit} style={{padding:"5px"}}> <b>Increase Products</b></button>
+            </div>
             <table className="table">
                 <TableHeaders />
                 <tbody>
